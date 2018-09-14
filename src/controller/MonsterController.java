8c2c30832aaacee8120c46311fdee53587f9e05f
Van hoffman <zvan1276@740s-pl-119.local>
2018-09-14 08:27:56 -0600

    added a catch for invalid integer and double inputs

--- src/controller/MonsterController.java
+++ src/controller/MonsterController.java
@@ -26,26 +26,31 @@ public class MonsterController
 	{
 		Scanner inputScanner;
 		
-		JOptionPane.showMessageDialog(null,  "show a message \nhere :3");
+		JOptionPane.showMessageDialog(null, "show a message \nhere :3");
 		//use this method instead of System.out.println("words words words");
 		
 		System.out.println("What is the name of your monster?");
+		String name = JOptionPane.showInputDialog(null, "What is the name of your monster?");
 		inputScanner = new Scanner(System.in);
 		String name = inputScanner.nextLine();
 		
 		System.out.println("How many legs will your monster have?");
+		double legCount = JOptionPane.showInputDialog(null, "How many legs will your monster have?");
 		inputScanner = new Scanner(System.in);
 		double legCount = inputScanner.nextDouble();
 		
-		System.out.println("What is the number of eyes your monster has?");
+		System.out.println("How many eyes does your monster have?");
+		int eyeCount = JOptionPane.showInputDialog(null, "How many eyes does your monster have?");
 		inputScanner = new Scanner(System.in);
 		int eyeCount = inputScanner.nextInt();
 		
 		System.out.println("Is your monster going to have any noses? true/false");
+		boolean hasNoses = JOptionPane.showInputDialog(null, "Is your monster going to have any noses? true/false");
 		inputScanner = new Scanner(System.in);
 		boolean hasNoses = inputScanner.nextBoolean();
 		
 		System.out.println("How many arms does your monster have?");
+		int armCount = JOptionPane.showInputDialog(null, "How many arms does your monster have?");
 		inputScanner = new Scanner(System.in);
 		int armCount = inputScanner.nextInt();
 		
@@ -56,10 +61,47 @@ public class MonsterController
 		JOptionPane.showMessageDialog(null,  myMonster);
 		myMonster.setArmCount(9999);
 		System.out.println("My monster has this many arms: " + myMonster.getArmCount() );
+		JOptionPane.showMessageDialog(null,"My monster has this many arms: " + myMonster.getArmCount());
 	
 		//Make a new monster and customize from user input
 		MarshmallowMonster userMonster;
 		userMonster = new MarshmallowMonster(name, legCount, eyeCount, hasNoses, armCount);
 		System.out.println(userMonster);
+		JOptionPane.showMessageDialog(null, userMonster);
 	}
+	
+	public boolean validInt(String maybeInt)
+	{
+		boolean isValid = false;
+		
+		try
+		{
+			Integer.parseInt(maybeInt);
+			isValid = true;
+		}
+		catch(NumberFormatException error)
+		{
+			JOptionPane.showMessageDialog(null, "Please type a valid\ninteger value");
+		}
+		
+		return isValid;
+	}
+	
+	public boolean validDouble(String maybeDouble)
+	{
+		boolean isValid = false;
+		
+		try
+		{
+			Double.parseDouble(maybeDouble);
+			isValid = true;
+		}
+		catch(NumberFormatException error)
+		{
+			JOptionPane.showMessageDialog(null, "This requires a double value");
+		}
+		
+		return isValid;
+	}
+	
 }
